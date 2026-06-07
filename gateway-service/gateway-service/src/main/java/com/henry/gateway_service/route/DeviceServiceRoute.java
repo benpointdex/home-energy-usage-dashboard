482b446efa80287bfd57a372f45c6217c2dd76bc
benpointdex <185585972+benpointdex@users.noreply.github.com>
2026-06-07 13:46:01 +0530

    feat: implemented the ckt breaker

---
+++ gateway-service/gateway-service/src/main/java/com/henry/gateway_service/route/DeviceServiceRoute.java
@@ -0,0 +1,39 @@
+package com.henry.gateway_service.route;
+
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class DeviceServiceRoute {
+
+    @Bean
+    public RouterFunction<ServerResponse> deviceServiceRoutes(){
+        return route("device-service")
+                .route(RequestPredicates.path("/api/v1/device/**"), http())
+                .before(uri("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("deviceServiceCircuitBreaker",
+                        URI.create("forward:/fallback/device"))
+                )
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> deviceFallbackRoutes(){
+        return route("deviceFallbackRoute")
+                .route(RequestPredicates.path("/fallback/device"),
+                        request->ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("device service is down"))
+                .build();
+    }
+}
